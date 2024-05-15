f7aebe3ad3c14035ccdc6233653c04d0ec30d1f1
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2024-05-15 16:38:20 -0400

    implementar devolver el número mismo para una cadena que contiene un solo número

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -4,5 +4,8 @@ describe('add', () => {
   it('deberia devolver 0 para una cadena vacia', () => {
     expect(add('')).toBe(0);
   });
+  it('debería devolver el número mismo para una cadena que contiene un solo número', () => {
+    expect(add('2')).toBe(2);
+  });
  
 });
